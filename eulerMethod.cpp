a822cfbf4bcf8f0f6d526ed02d5f72277b4ef66a
Devexhh <krsujal2004@gmail.com>
2024-11-06 16:46:16 +0530

    Update eulerMethod.cpp

--- eulerMethod.cpp
+++ eulerMethod.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 double f(double x, double y) {
-    return -2*x*y*y;
+    return x+y;
 }
 
 void eulerMethod(double x0, double y0, double h, double xf) {
     double x = x0;
     double y = y0;
-    while (x < xf) {
+    while (x < xf-h) {
+        if (x == 1.0) break;
         y += h*f(x,y);
         x += h;
     }
@@ -16,10 +17,10 @@ void eulerMethod(double x0, double y0, double h, double xf) {
 }
 
 int main () {
-    double x0 = 1.0;
+    double x0 = 0.0;
     double y0 = 1.0;
     double h = 0.1;
-    double xf = 1.3;
+    double xf = 1.0;
     eulerMethod(x0, y0,h,xf);
     return 0;
 }
